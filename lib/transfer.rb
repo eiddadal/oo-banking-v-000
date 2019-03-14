52591fac6f00bb9492f8d7b48e6581e2325f11cb
Dalya Eidda <dalia.eidda@gmail.com>
2019-03-14 01:50:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,24 @@
 class Transfer
-  # your code here
+  attr_accessor :status, :amount
+  attr_reader :sender, :receiver
+  
+ def initialize(sender, receiver, amount)
+   @sender = sender 
+   @receiver = receiver
+   @status = "pending"
+   @amount = amount
+ end 
+ 
+ def valid? 
+   if @sender.valid? == true && @receiver.valid? == true 
+     true
+   end 
+ end 
+ 
+ def execute_transaction
+ end 
+ 
+ def reverse_transfer 
+ end 
+ 
 end
